a43ea1f1dcd809455dd05d03087bba065088ecb9
Viacheslav Sipakov <sipakov.v@rambler.ru>
2021-12-03 10:08:06 +0300

    Added csv reading

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import threading
+import csv
+import sys
 
-print('хрень какая-то')
+
+filename = 'data.csv'
+data = []
+with open(filename, newline='') as f:
+    reader = csv.reader(f)
+    try:
+        for row in reader:
+            #print(row)
+            data.append(row)
+    except csv.Error as e:
+        sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
+
+print(data)
+
