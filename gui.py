a03177cb0b34ca923b6680455efa2600badd0289
Kirill <kirillm181@gmail.com>
2021-12-05 20:08:28 +0300

    added buttons without function

--- gui.py
+++ gui.py
@@ -17,6 +17,18 @@ class App(tk.Frame):
         self.contents = tk.StringVar()
         # Set it to some value.
         self.contents.set('this is a variable')
+        
+        #Ввожу кнопки, пока они нефункциональны (Aksolot), чтобы привязать их к функциям на определённые действия, используйте аргумент command функции tk.Button
+        self.btn = tk.Button(master, text = "Forward", activebackground='#eeeeee',activeforeground='#000000',bg='#a0a000',fg='#ffffff',width=10)
+        self.btn.place(x=10, y=50)
+        self.bt2 = tk.Button(master, text = "Backward", activebackground='#eeeeee',activeforeground='#000000',bg='#a0a000',fg='#ffffff',width=10)
+        self.bt2.place(x=160, y=50)
+        self.bt3 = tk.Button(master, text = "Refresh", activebackground='#eeeeee',activeforeground='#000000',bg='#00a000',fg='#ffffff',width=10)
+        self.bt3.place(x=310, y=50)
+        self.bt4 = tk.Button(master, text = "Quit", activebackground='#eeeeee',activeforeground='#000000',bg='#a00000',fg='#ffffff',width=10)
+        self.bt4.place(x=460, y=50)
+        
+        
         # Tell the entry widget to watch this variable.
         self.entrythingy['textvariable'] = self.contents
 
